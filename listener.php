cd3f36436b5e0cbab66885b668876a5f7575b5bd
max <maxdml@cs.duke.edu>
2014-12-26 01:40:49 -0500

    php listener

---
+++ listener.php
@@ -0,0 +1,15 @@
+<?php
+
+$str_json = file_get_contents('php://input') . "\n";
+error_log("raw post data: $str_json");
+
+$handle = fopen('/var/www/html/bandwidth/raw_data.json', 'a') or die ('Can\'t open file' . "\n");
+
+if (!fwrite($handle, $str_json)) {
+    error_log("could not write to file $handle");;
+    exit;
+}
+
+fclose($handle);
+
+?>
