30fe115bae36401a85cc8d680d28eea7a6a0693a
max <maxdml@cs.duke.edu>
2014-12-26 19:45:15 -0500

    implement mean calculus for gathered data

--- README.md
+++ README.md
@@ -8,9 +8,9 @@ Howto:
 
 - setup an apache server with mod_ssl activated
 - put the listener in an accessible directory bandwidth/listener (the data are sent to https://127.0.0.1/bandwidth/listener.php)
-- create a file named raw_data and set it to 777 (I know :)
+- create a file named raw_data in the listener directory and set it to 777 (I know :)
 - install chrome extension page load time (https://chrome.google.com/webstore/detail/page-load-time/fploionmjgeclbkemipmkogoaohcdbig)
-- apply the patch timer.js.patch on the extension file timer.js (on Linux, at least Fedora, one can find it under ~/.config/google-chrome/Default/Extensions/fploionmjgeclbkemipmkogoaohcdbig/1.2.2_0)
+- apply the patch timer.patch on the extension file timer.js (on Linux, at least Fedora, one can find it under ~/.config/google-chrome/Default/Extensions/fploionmjgeclbkemipmkogoaohcdbig/1.2.2_0)
 
 Since this last step, Chrome should start sending data about tab's loading time to the listener (note: chrome has to be launched in CLI with the option --ignore-certificate-errors)
 
--- bandwidth.py
+++ bandwidth.py
@@ -7,6 +7,7 @@ from subprocess import call
 from time import gmtime, strftime
 import time
 import re
+import pdb
 
 parser = argparse.ArgumentParser(description='Tool to gather load page time data.')
 
@@ -19,10 +20,11 @@ parser.add_argument('runs', help='the number of run you want to perform', type=i
 args = parser.parse_args()
 
 #Modify constants here
-rawdata = '/var/www/html/bandwidth/raw_data'
-wd      = '/home/max/codeZ/bandwidth/'
-data    =  wd + 'data.json'
-chrome  = '/opt/google/chrome/google-chrome'
+rawdata    = '/var/www/html/bandwidth/raw_data'
+wd         = '/home/max/codeZ/bandwidth/'
+data       =  wd + 'data.json'
+chrome     = '/opt/google/chrome/google-chrome'
+serie_time = strftime("%Y-%m-%d_%H:%M:%S", gmtime())
     
 def requester():
     #get url list and call them through chrome
@@ -53,7 +55,7 @@ def requester():
             subprocess.call('/opt/google/chrome/google-chrome --ignore-certificate-errors ' + url,
                             shell=True);
 
-        #make sure that all pages have been load to manipulate raw_data.json
+        #FIXME: make sure that all pages have been load to manipulate raw_data.json
         time.sleep(8)
             
         #parse raw_data and build a structure to calculate the mean 
@@ -64,22 +66,47 @@ def requester():
             lines = rd.readlines()
             for line in range(offset + 1, offset + url_nb + 1):
                 #print 'reading from ' + str(offset + 1) + ' to ' + str(offset + url_nb)
-                parsed    = lines[line].rstrip('\r\n')
-                sparsed   = re.split(' : ', parsed)
-                data_serie[args.bwrate][index_run][sparsed[0]] = sparsed[1]
+                parsed     = lines[line].rstrip('\r\n')
+                sparsed    = re.split(' : ', parsed)
+                format_url = re.sub('(http)s?://', '', sparsed[0])
+                format_url = format_url.rstrip('/')
+                data_serie[args.bwrate][index_run][format_url] = sparsed[1]
 
     #create a backup for the runs serie
-    backup_file = wd + args.bwrate + '_' + strftime("%Y-%m-%d_%H:%M:%S", gmtime())
+    backup_file = wd + args.bwrate + '_all_runs_' + serie_time
     cmd = 'touch ' + backup_file
     subprocess.call(cmd, shell=True);
     with open(backup_file, 'w') as bck:
          bck.write(json.dumps(data_serie))
 
-    return 1
+    return data_serie
 
 
-def operateMean():
-    return 1
+def operateMean(data):
+    #gather data for each url over all the runs and compute mean
+    data_mean = {}
+    data_mean[args.bwrate] = {}
+    with open(args.urlfile, 'r') as uf:
+        urlz = uf.readlines()
+        for url in urlz:
+            url = url.rstrip('\r\n')
+            temp = 0.000 
+            for run in range(1, args.runs + 1):
+                temp += float(data[args.bwrate][run][url])
+   
+            mean_lt = temp / run
+            print 'mean for ' + url + ' is ' + str(mean_lt)
+            data_mean[args.bwrate][url] = str(mean_lt)
+   
+    #save the results into a file
+    result_file = wd + args.bwrate + '_mean_' + serie_time 
+    cmd = 'touch ' + result_file
+    subprocess.call(cmd, shell=True)
+    with open(result_file, 'w') as res:
+        res.write(json.dumps(data_mean))
+
+    return data_mean
 
 
-requester()
+data      = requester()
+mean_data = operateMean(data)
